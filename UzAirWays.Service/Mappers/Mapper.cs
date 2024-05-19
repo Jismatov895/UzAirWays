6902442046bb41a9ff053f629d69ad45842eb8d0
Jismatov895 <Jismatov895@gmail.com>
2024-05-20 03:58:27 +0500

    Added Airport Service and Interface

--- UzAirWays.Service/Mappers/Mapper.cs
+++ UzAirWays.Service/Mappers/Mapper.cs
@@ -1,4 +1,5 @@
-﻿using ToshiChilonzor.Domain.Entities;
+﻿using System.Net.Sockets;
+using ToshiChilonzor.Domain.Entities;
 using UzAirWays.Domain.Entities;
 using UzAirWays.Service.DTOs.Airports;
 using UzAirWays.Service.DTOs.Planes;
@@ -105,20 +106,16 @@ public static class Mapper
         };
     }
 
-    public static IEnumerable<AirportViewModel> Map(List<Airport> airport)
+    public static IEnumerable<AirportViewModel> Map(IEnumerable<Airport> airports)
     {
-        var result = new List<AirportViewModel>();
-        airport.ForEach(airport => result.Add(new AirportViewModel
+        return airports.Select( airport => new AirportViewModel
         {
             Id = airport.Id,
             Name = airport.Name,
             Country = airport.Country,
             City = airport.City,
             Address = airport.Address,
-
-        }));
-
-        return result;
+        });
     }
     public static Flight Map(FlightCreateModel createModel)
     {
