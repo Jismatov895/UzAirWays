e53f48474e689ccae7b53345ed7699299d0ccda2
Jismatov895 <Jismatov895@gmail.com>
2024-05-19 22:22:55 +0500

    Added Ticket DTO

---
+++ UzAirWays.Service/DTOs/Tickets/TicketUpdateModel.cs
@@ -0,0 +1,12 @@
+﻿using UzAirWays.Domain.Enums;
+
+namespace UzAirWays.Service.DTOs.Tickets;
+
+public class TicketUpdateModel
+{
+    public long FlightId { get; set; }
+    public long UserId { get; set; }
+    public SeedStatus SeedStatus { get; set; }
+    public decimal Price { get; set; }
+}
+
---
+++ UzAirWays.Service/DTOs/Tickets/TicketViewModel.cs
@@ -0,0 +1,14 @@
+﻿using ToshiChilonzor.Domain.Entities;
+using UzAirWays.Domain.Enums;
+using UzAirWays.Service.DTOs.Users;
+
+namespace UzAirWays.Service.DTOs.Tickets;
+
+public class TicketViewModel
+{
+    public FlightViewModel Flight { get; set; }
+    public UserViewModel User { get; set; }
+    public SeedStatus SeedStatus { get; set; }
+    public decimal Price { get; set; }
+}
+
