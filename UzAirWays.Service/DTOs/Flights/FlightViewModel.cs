e55c84cc0713060522a26024dd003468b03e7591
axrordev <axror.algo@gmail.com>
2024-05-19 20:43:03 +0500

    created flight dtos

---
+++ UzAirWays.Service/DTOs/Flights/FlightViewModel.cs
@@ -0,0 +1,15 @@
+﻿
+namespace ToshiChilonzor.Domain.Entities;
+
+public class FlightViewModel
+{
+    public long Id { get; set; }
+    public PlaneViewModel Plane { get; set; }
+    public AirportViewModel FirstAirport { get; set; }
+    public AirportViewModel LastAirport { get; set; }
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+    public decimal EconomPrice { get; set; }
+    public decimal BusinessPrice { get; set; }
+    public ICollection<TicketViewModel> Tickets { get; set; }
+}
