4b0b17198dbdd0ce4f4ac9126f2baa4680b267fc
axrordev <axror.algo@gmail.com>
2024-05-20 00:12:50 +0500

    added user repository

---
+++ UzAirWays.DataAccess/Repositories/Users/UserRepository.cs
@@ -0,0 +1,50 @@
+﻿using System.Numerics;
+using UzAirWays.Domain.Entities;
+
+namespace UzAirWays.DataAccess.Repositories.Users;
+
+public class UserRepository : IUserRepository
+{
+    private AppDbContext context;
+    public UserRepository(AppDbContext context)
+    {
+        this.context = context;
+    }
+    public async Task<User> InsertAsync(User user)
+    {
+        return (await context.Users.AddAsync(user)).Entity;
+    }
+
+    public async Task<User> UpdateAsync(User user)
+    {
+        context.Users.Update(user);
+        return await Task.FromResult(user);
+    }
+
+    public async Task<User> DeleteAsync(User user)
+    {
+        user.IsDeleted = true;
+        context.Users.Update(user);
+        return await Task.FromResult(user);
+    }
+
+    public async Task<User> SelectAsync(long id)
+    {
+        return await context.Users.FindAsync(id);
+    }
+    public async Task<IEnumerable<User>> SelectAllAsEnumerableAsync()
+    {
+        return await Task.FromResult(context.Users.Where(user => !user.IsDeleted));
+    }
+
+    public async Task<IQueryable<User>> SelectAllAsQuerableAsync()
+    {
+        return await Task.FromResult(context.Users.AsQueryable().Where(user => !user.IsDeleted));
+    }
+
+    public async Task<bool> SaveAsync()
+    {
+        return (await context.SaveChangesAsync()) > 0;
+    }
+
+}
--- UzAirWays.Domain/Entities/User.cs
+++ UzAirWays.Domain/Entities/User.cs
@@ -7,4 +7,5 @@ public class User : Auditable
     public string LastName { get; set; }
     public string Email { get; set; }
     public string Password { get; set; }
+    // public ICollection<Flight> Flights {get; set;}   i wanna add this collection but team dont want 
 }
