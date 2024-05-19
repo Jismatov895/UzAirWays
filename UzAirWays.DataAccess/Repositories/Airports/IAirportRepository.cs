c8bb3c01f2cc71207f2561698646f042eacb275d
axrordev <axror.algo@gmail.com>
2024-05-20 00:37:21 +0500

    created airport repo

---
+++ UzAirWays.DataAccess/Repositories/Airports/IAirportRepository.cs
@@ -0,0 +1,14 @@
+﻿using UzAirWays.Domain.Entities;
+
+namespace UzAirWays.DataAccess.Repositories.Airports;
+
+public interface IAirportRepository
+{
+    Task<Airport> InsertAsync(Airport airport);
+    Task<Airport> UpdateAsync(Airport airport);
+    Task<Airport> DeleteAsync(Airport airport);
+    Task<Airport> SelectAsync(long id);
+    Task<IEnumerable<Airport>> SelectAllAsEnumerableAsync();
+    Task<IQueryable<Airport>> SelectAllAsQuerableAsync();
+    Task<bool> SaveAsync();
+}
