4eaf8b43aba9e04f5be9494a398832aba5f4012f
axrordev <axror.algo@gmail.com>
2024-05-20 01:28:08 +0500

    created fligth repo

---
+++ UzAirWays.DataAccess/Repositories/Flights/IFlightRepository.cs
@@ -0,0 +1,14 @@
+﻿using UzAirWays.Domain.Entities;
+
+namespace UzAirWays.DataAccess.Repositories.Flights;
+
+public interface IFlightRepository
+{
+    Task<Flight> InsertAsync(Flight flight);
+    Task<Flight> UpdateAsync(Flight flight);
+    Task<Flight> DeleteAsync(Flight flight);
+    Task<Flight> SelectAsync(long id, string[] includes = null);
+    Task<IEnumerable<Flight>> SelectAllAsEnumerableAsync(string[] includes = null, bool isTraking = true);
+    Task<IQueryable<Flight>> SelectAllAsQuerableAsync(string[] includes = null, bool isTraking = true);
+    Task<bool> SaveAsync();
+}
