0017b355f3abe64c24c954eeb8a9acec90f03656
axrordev <axror.algo@gmail.com>
2024-05-19 20:31:37 +0500

    created Plane enttity

---
+++ UzAirWays.Domain/Entities/Plane.cs
@@ -0,0 +1,10 @@
+﻿using UzAirWays.Domain.Commons;
+
+namespace UzAirWays.Domain.Entities;
+
+public class Plane : Auditable
+{
+    public string Number { get; set; }
+    public int EconomSeats { get; set; }
+    public int BusinessSeats { get; set; }
+}
