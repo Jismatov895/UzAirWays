4dfa1e60a92ca2d597515a6404ce455c5cab3cc0
Jismatov895 <Jismatov895@gmail.com>
2024-05-19 21:25:37 +0500

    Added AppDbContext

---
+++ UzAirWays.DataAccess/Contexts/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UzAirWays.Domain.Entities;
+public class AppDbContext : DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseNpgsql("Server=localhost; Database=UzAirway; Port=5432; User ID=postgres; Password=");
+    }
+
+    public DbSet<User> Users { get; set; }
+    public DbSet<Airport> Airports { get; set; }
+    public DbSet<Plane> Planes { get; set; }
+    public DbSet<Flight> Flights { get; set; }
+    public DbSet<Ticket> Tickets { get; set; }
+}
