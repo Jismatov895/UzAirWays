b68b5af765df48e9f2c8c505562d04ce9a12bfbb
Jismatov895 <Jismatov895@gmail.com>
2024-05-19 19:34:27 +0500

    Added Airport DTO

---
+++ UzAirWays.Service/DTOs/Airports/AirportUpdateModel.cs
@@ -0,0 +1,8 @@
+﻿namespace UzAirWays.Service.DTOs.Airports;
+public class AirportUpdateModel
+{
+    public string Name { get; set; }
+    public string Country { get; set; }
+    public string City { get; set; }
+    public string Address { get; set; }
+}
